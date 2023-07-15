bd7c657a280f0609d41f3fee60ee878e2bffdbcb
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-07-15 19:53:08 +0530

    level 4 security hash with salt added

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-// md5 library for hashing
-const md5 = require('md5');
-
+// bcrpypt library for salting + stronger hashing
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
 // console.log(process.env.API_KEY);
-
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -47,12 +46,14 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', async(req, res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
 
     try {
+      // using bcrypt to hash passwords
+      const hash = await bcrypt.hash(req.body.password, saltRounds)
+      const newUser = new User({
+        email: req.body.username,
+        password: hash,
+      })
         await newUser.save();
         res.render("secrets")
     } catch (error) {
@@ -62,11 +63,16 @@ app.post('/register', async(req, res)=>{
 
 app.post('/login', async (req,res)=>{
   const username = req.body.username;
-  const password = md5(req.body.password)
+  const password = req.body.password
 
   try {
     const foundUser = await User.findOne({email: username})
-    if(foundUser.password === password){
+    // if(foundUser.password === password)
+    // changing normal password authentication to bcrypt
+    const result = await bcrypt.compare(password,foundUser.password);
+    // foundUser.password contains the hash
+    if(result === true)
+    {
       res.render("secrets");
     }
 

293435c57997781a319c94e099d95045b93f8772
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-07-17 13:32:07 +0530

    changes for deployment

--- app.js
+++ app.js
@@ -193,6 +193,10 @@ app.get('/logout', async (req,res)=>{
 
 
 
-app.listen(3000, ()=>{
-    console.log("Server running on port 3000");
+let port = process.env.PORT
+if (port == null || port == '') {
+  port = 3000
+}
+app.listen(port, function () {
+  console.log('Server started successfully on port ' + port)
 })
